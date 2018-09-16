ac935589eebf6d45fae25a6b55ae0154890e6ffe
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2018-09-16 14:54:43 +0100

    Sort by title

--- src/CardsContainer.js
+++ src/CardsContainer.js
@@ -2,18 +2,24 @@ import React, { Component } from 'react';
 import Card from './Card.js';
 import './CardsContainer.css';
 
-const photosURL = 'https://jsonplaceholder.typicode.com/photos?_limit=9&_page=1&_sort=title&_order=asc'
+const photosURL = 'https://jsonplaceholder.typicode.com/photos?_limit=9&_page=1&_sort=title&_order='
 const chunkSize = 3;
 
 class CardsContainer extends Component {
 
   constructor() {
     super();
-    this.state = { cards: [] }
+    this.state = {
+      cards: [],
+      order: 'asc'
+    }
+
+    this.sort = this.sort.bind(this);
   }
 
-  fetchPhotos() {
-    return fetch(photosURL)
+  fetchPhotos(order) {
+    const sort_order = order || this.state.order;
+    return fetch(photosURL + sort_order)
       .then(res => res.json())
       .catch(error => { console.log(error) });
   }
@@ -24,9 +30,18 @@ class CardsContainer extends Component {
    })
   }
 
+  sort() {
+    const order = this.state.order === 'asc' ? 'desc' : 'asc';
+    console.log(order);
+    this.fetchPhotos(order).then(res => {
+      this.setState({cards: res.chunk(chunkSize), order: order})
+    });
+  }
+
   render() {
     return (
       <div id='cards_container'>
+        <a href='#' onClick={this.sort}>Title</a>
         <div id='cards'>
           {
             this.state.cards.slice(0,2).map((cards, index) => (
