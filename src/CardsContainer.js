e7ff69ea5302d25516f0d0d08cb793edc1efbd5d
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2018-09-17 22:21:45 +0100

    Assume sorting is about fetched elements, not extra request

--- src/CardsContainer.js
+++ src/CardsContainer.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Card from './Card.js';
 import './CardsContainer.css';
 
-const photosURL = 'https://jsonplaceholder.typicode.com/photos?_limit=9&_page=1&_sort=title&_order='
+const photosURL = 'https://jsonplaceholder.typicode.com/photos?_limit=60&_page=1&_sort=title&_order='
 const chunkSize = 3;
 const rows = 2;
 
@@ -35,10 +35,20 @@ class CardsContainer extends Component {
   sort() {
     const order = this.state.order === 'asc' ? 'desc' : 'asc';
     console.log(order);
-    this.fetchPhotos(order).then(res => {
-      const cards = res.chunk(chunkSize);
-      this.setState({cards: cards, order: order})
-    });
+    const cards = [].concat.apply([],this.state.cards).sort(this.compare(order))
+    this.setState({cards: cards.chunk(chunkSize), order: order})
+  }
+
+  compare(order) {
+    return (a,b) => {
+      if (a.title < b.title) {
+          return order === 'asc' ? -1 : 1;
+      }
+      if (a.title > b.title) {
+          return order === 'asc' ? 1 : -1;
+      }
+      return 0;
+    }
   }
 
   sortIcon() {
